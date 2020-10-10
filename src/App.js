5e9a1aa774f7964c3f625ca81129e75c0496811b
mdipanjan <mdipanjan33@gmail.com>
2020-10-10 21:17:22 +0530

    sign-in-added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import Amplify from "aws-amplify";
+import Amplify, { API } from "aws-amplify";
 import awsconfig from "./aws-exports";
-import { Auth } from "aws-amplify";
+import { Auth } from "@aws-amplify/auth";
 
 Amplify.configure(awsconfig);
 
@@ -11,18 +11,42 @@ const initialFormState = {
   username: "",
   password: "",
   email: "",
-  authCode: "",
-  formType: "signUp",
+  code: "",
+  formType: "signIn",
 };
 
 function App() {
   const [formState, updateFormState] = useState(initialFormState);
+  const [user, updateUser] = useState(null);
   let { formType } = formState;
+
+  useEffect(() => {
+    getCurrentUserstatus();
+  }, []);
+  const getCurrentUserstatus = async () => {
+    try {
+      let userStat = await Auth.currentAuthenticatedUser();
+      updateFormState(() => ({ ...formState, formType: "signedIn" }));
+      console.log(userStat);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  async function checkUser() {
+    try {
+      let user = await API.currentAuthenticatedUser();
+      console.log(user);
+    } catch (err) {
+      console.log(err);
+    }
+  }
   function handleChange(e) {
     updateFormState({ ...formState, [e.target.name]: e.target.value });
   }
+
   async function handleSignUpSubmit(e) {
     e.preventDefault();
+
     let { username, password, email } = formState;
     try {
       const { user } = await Auth.signUp({
@@ -32,9 +56,34 @@ function App() {
           email, // optional
         },
       });
+      updateFormState(() => ({ ...formState, formType: "confirmSignUp" }));
       console.log(user);
     } catch (error) {
       console.log("error signing up:", error);
+    }
+  }
+
+  async function handleConfirmSignup(e) {
+    e.preventDefault();
+
+    let { formType, username, code } = formState;
+    try {
+      await Auth.confirmSignUp(username, code);
+      updateFormState(() => ({ ...formState, formType: "signIn" }));
+    } catch (error) {
+      console.log("error confirming sign up", error);
+    }
+  }
+
+  async function handleSignIn(e) {
+    e.preventDefault();
+
+    let { username, password, email } = formState;
+    try {
+      const user = await Auth.signIn(username, password);
+      updateFormState(() => ({ ...formState, formType: "signedIn" }));
+    } catch (error) {
+      console.log("error signing in", error);
     }
   }
 
@@ -93,6 +142,72 @@ function App() {
             </button>
           </form>
         )}
+        {formType === "confirmSignUp" && (
+          <form
+            onSubmit={(e) => {
+              handleConfirmSignup(e);
+            }}
+          >
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Entre your code</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Your code"
+                name="code"
+                value={formState.code}
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+              />
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Verify
+            </button>
+          </form>
+        )}
+        {formType === "signIn" && (
+          <form
+            className="mt-5"
+            onSubmit={(e) => {
+              handleSignIn(e);
+            }}
+          >
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="User name"
+                name="username"
+                value={formState.username}
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                name="password"
+                className="form-control"
+                placeholder="Password"
+                value={formState.password}
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+              />
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        )}
+        {formType === "signedIn" && <h1>Inside App! Hello world</h1>}
       </div>
     </div>
   );
